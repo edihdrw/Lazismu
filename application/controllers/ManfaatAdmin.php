a721b9e0b66d09e0bbd1851a516314b0bad9ff00
edihdrw <edihendriawan110@gmail.com>
2022-07-11 03:27:16 +0700

    Modified validate tanggal lahir

--- application/controllers/ManfaatAdmin.php
+++ application/controllers/ManfaatAdmin.php
@@ -52,15 +52,15 @@ class ManfaatAdmin extends CI_Controller
             echo "
             <div class=\"form-group\">
                                     <label>Nama</label>
-                                    <input name=\"nama\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\">
+                                    <input name=\"nama\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\" required>
                                 </div>
                                 <div class=\"form-group\">
                                     <label>NIK </label>
-                                    <input name=\"nik\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\">
+                                    <input name=\"nik\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\" required>
                                 </div>
                                 <div class=\"form-group\">
                                     <label>Nomer KK</label>
-                                    <input name=\"kk\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\">
+                                    <input name=\"kk\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\" required>
                                 </div>
 
                                 <div class=\"form-group\">
@@ -75,7 +75,7 @@ class ManfaatAdmin extends CI_Controller
 
                                 <div class=\"form-group\">
                                     <label>Tempat Lahir</label>
-                                    <input name=\"tmp_lahir\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\">
+                                    <input name=\"tmp_lahir\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\" required>
                                 </div>
 
                                 <div class='form-group'>
@@ -107,10 +107,10 @@ class ManfaatAdmin extends CI_Controller
 
                                 <div class=\"form-group\">
                                     <label>Pekerjaan</label>
-                                    <input name=\"pekerjaan\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\">
+                                    <input name=\"pekerjaan\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\" required>
                                 </div><div class=\"form-group\">
                                     <label>Nomer HP</label>
-                                    <input name=\"hp\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\">
+                                    <input name=\"hp\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\" required>
                                 </div>
             
             ";
@@ -118,19 +118,19 @@ class ManfaatAdmin extends CI_Controller
             echo "
             <div class=\"form-group\">
                                     <label>Nama</label>
-                                    <input name = \"nama\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\">
+                                    <input name = \"nama\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\" required>
                                 </div>
                                 <div class=\"form-group\">
                                     <label>NIK </label>
-                                    <input name=\"nik\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\">
+                                    <input name=\"nik\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\" required>
                                 </div>
                                 <div class=\"form-group\">
                                     <label>Nomer KK</label>
-                                    <input name=\"kk\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\">
+                                    <input name=\"kk\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\" required>
                                 </div>
                                 </div><div class=\"form-group\">
                                     <label>Nomer HP</label>
-                                    <input name=\"hp\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\">
+                                    <input name=\"hp\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\" required>
                                 </div>
                              
             
@@ -139,20 +139,20 @@ class ManfaatAdmin extends CI_Controller
             echo "
             <div class=\"form-group\">
                                     <label>Nama Lembaga</label>
-                                    <input type=\"text\" name = \"nama_lembaga\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\">
+                                    <input type=\"text\" name = \"nama_lembaga\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\" required>
                                 </div>
                                  <div class=\"form-group\">
                                     <label>Nama PIC</label>
-                                    <input name = \"nama\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\">
+                                    <input name = \"nama\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\" required>
                                 </div>
                                 <div class=\"form-group\">
                                     <label>NIK PIC </label>
-                                    <input name=\"nik\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\">
+                                    <input name=\"nik\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\" required>
                                 </div>
                               
                                 </div><div class=\"form-group\">
                                     <label>Nomer HP</label>
-                                    <input name=\"hp\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\">
+                                    <input name=\"hp\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\" required>
                                 </div>
                              
             
@@ -161,21 +161,21 @@ class ManfaatAdmin extends CI_Controller
             echo "
             <div class=\"form-group\">
                                     <label>Nama Usaha</label>
-                                    <input type=\"text\" name = \"nama_lembaga\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\">
+                                    <input type=\"text\" name = \"nama_lembaga\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\" required>
                                 </div>
                                  <div class=\"form-group\">
                                     <label>Nama PIC</label>
-                                    <input name = \"nama\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\">
+                                    <input name = \"nama\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\" required>
                                 </div>
                              
                                 <div class=\"form-group\">
                                     <label>NIK PIC </label>
-                                    <input name=\"nik\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\">
+                                    <input name=\"nik\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\" required>
                                 </div>
                                    <small class=\"text-danger\"><?= form_error('nik'); ?></small>
                                 </div><div class=\"form-group\">
                                     <label>Nomer HP</label>
-                                    <input name=\"hp\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\">
+                                    <input name=\"hp\" type=\"text\" class=\"form-control\" placeholder=\"Enter ...\" id=\"formjiwa\" required>
                                 </div>
                              
             
@@ -186,7 +186,7 @@ class ManfaatAdmin extends CI_Controller
     public function tambah_manfaat()
     {
 
-        $this->form_validation->set_rules('nik', 'NIK', 'uniq|required|min_length[16]|max_length[16]', [
+        $this->form_validation->set_rules('nik', 'NIK', 'required|min_length[16]|max_length[16]', [
 
             'min_length' => 'NIK Harus 16 karakter',
             'max_length' => 'NIK Harus 16 karakter'
@@ -205,6 +205,8 @@ class ManfaatAdmin extends CI_Controller
 
             redirect('manfaatadmin');
         } else {
+
+
             $sumber = $this->input->post('sumber');
             $asnaf = $this->input->post('asnaf');
             $kategori = $this->input->post('kategori');
@@ -230,44 +232,56 @@ class ManfaatAdmin extends CI_Controller
 
 
             if ($kategori == 1) {
-                $data = [
-                    'sumber' => $sumber,
-                    'asnaf' => $asnaf,
-                    'kategori_manfaat' => $kategori,
-                    'nik' => $nik,
-                    // 'm_nik_keluarga' => $nik,
-                    // 'm_nik_lembaga' => $nik,
-                    'nama_manfaat' => $nama,
-                    'hp' => $hp,
-                    'm_provinsi' => $provinsi,
-                    'm_kota' => $kota,
-                    'm_kecamatan' => $kecamatan,
-                    'm_desa' => $desa,
-                    'alamat' => $alamat
-
-                ];
-                $this->manfaat_m->tambah('tb_manfaat', $data);
-
-                $data1 = [
 
 
-                    'nik_jiwa' => $nik,
-                    'kk' => $kk,
-                    'kelamin' => $kelamin,
-                    'tmp_lahir' => $tmp_lahir,
-                    'tanggal' => $tanggal,
-                    'pendidikan' => $pendidikan,
-                    'pekerjaan' => $pekerjaan,
+
+                if (strtotime($tanggal) >= time()) {
+
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                Periksa ulang tanggal lahir anda!
+                </div>');
+
+                    redirect('manfaatadmin');
+                } else {
+                    $data = [
+                        'sumber' => $sumber,
+                        'asnaf' => $asnaf,
+                        'kategori_manfaat' => $kategori,
+                        'nik' => $nik,
+                        // 'm_nik_keluarga' => $nik,
+                        // 'm_nik_lembaga' => $nik,
+                        'nama_manfaat' => $nama,
+                        'hp' => $hp,
+                        'm_provinsi' => $provinsi,
+                        'm_kota' => $kota,
+                        'm_kecamatan' => $kecamatan,
+                        'm_desa' => $desa,
+                        'alamat' => $alamat
+
+                    ];
+                    $this->manfaat_m->tambah('tb_manfaat', $data);
+
+                    $data1 = [
 
 
-                ];
+                        'nik_jiwa' => $nik,
+                        'kk' => $kk,
+                        'kelamin' => $kelamin,
+                        'tmp_lahir' => $tmp_lahir,
+                        'tanggal' => $tanggal,
+                        'pendidikan' => $pendidikan,
+                        'pekerjaan' => $pekerjaan,
 
-                $this->manfaat_m->tambah('jiwa', $data1);
 
-                $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+                    ];
+
+                    $this->manfaat_m->tambah('jiwa', $data1);
+
+                    $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
                 Data Berhasil ditambah!
                 </div>');
-                redirect('manfaatadmin');
+                    redirect('manfaatadmin');
+                }
             } elseif ($kategori == 2) {
                 $data = [
                     'sumber' => $sumber,
